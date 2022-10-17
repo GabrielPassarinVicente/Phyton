4aa90f373611483b462946b1b040ce6a9b4d6b7f
gabriel <gab.gpv45@gmail.com>
2022-10-17 15:12:56 -0300

    Primeiro commit

---
+++ Exercicio1 .py
@@ -0,0 +1,6 @@
+# Exercício número 1
+#Converter Metros em cm
+
+metros = int(input('Informe o valor em metros: '))
+print('O valor em centrimetros é :', metros * 100)
+
---
+++ Exercicio2.py
@@ -0,0 +1,8 @@
+#Faça um algoritmo que receba duas notas e calcule a média
+
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2)/2
+
+print('a media e : ', media)
